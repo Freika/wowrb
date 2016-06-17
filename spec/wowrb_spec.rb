fd3b2ed749d31f7e21c478e428a3f26b3dff34f7
Arthur <zhenya.brovcko@yandex.ru>
2016-06-17 15:29:02 -0700

    Added rspec testing

--- spec/wowrb_spec.rb
+++ spec/wowrb_spec.rb
@@ -1,26 +1,44 @@
 require 'spec_helper'
 
-describe Wowrb do
+  describe Wowrb do
+  
   it 'has a version number' do
     expect(Wowrb::VERSION).not_to be nil
   end
 
   it 'has a key value' do 
-    ENV['BATTLE_NET_KEY'] = 'key'
+    expect(Wowrb.config.api_key).not_to eq('')
     expect(Wowrb.config.api_key).not_to be nil
   end
 
   it 'has a region value' do 
-    ENV['BATTLE_NET_REGION'] = 'RU'
+    expect(Wowrb.config.region).not_to eq('')
     expect(Wowrb.config.region).not_to be nil
   end
 
-  it 'has a locale value' do
-    ENV['BATTLE_NET_LOCALE'] = 'ru_RU'
+  it 'has a locale value' do 
+    expect(Wowrb.config.locale).not_to eq('')
     expect(Wowrb.config.locale).not_to be nil
+  end 
+
+  it 'no has a key value' do     
+    expect(Wowrb.region_leaderboard).to eq("Env variables missing: ENV['BATTLE_NET_KEY']")
+  end
+
+  it 'no has a region value' do     
+    expect(Wowrb.region_leaderboard).to eq("Env variables missing: ENV['BATTLE_NET_REGION']")
+  end
+
+  it 'no has a locale value' do     
+    expect(Wowrb.region_leaderboard).to eq("Env variables missing: ENV['BATTLE_NET_LOCALE']")
+  end
+
+  it 'no has all value' do     
+    expect(Wowrb.region_leaderboard).to eq("Env variables missing: ENV['BATTLE_NET_KEY'], ENV['BATTLE_NET_REGION'], ENV['BATTLE_NET_LOCALE']")
   end
 
   it 'does something useful' do
     expect(false).to eq(true)
   end
-end
+
+  end
