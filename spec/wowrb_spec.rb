d245cfc7a2f91f59a561465d1ae75e150ff17d68
Arthur <zhenya.brovcko@yandex.ru>
2016-06-20 07:08:05 -0700

    Added rspec testing

--- spec/wowrb_spec.rb
+++ spec/wowrb_spec.rb
@@ -5,21 +5,6 @@ describe Wowrb do
     expect(Wowrb::VERSION).not_to be_nil
   end
 
-  it 'has a key value' do 
-    expect(Wowrb.config.api_key).not_to be_empty
-    expect(Wowrb.config.api_key).not_to be_nil
-  end
-
-  it 'has a region value' do 
-    expect(Wowrb.config.region).not_to be_empty
-    expect(Wowrb.config.region).not_to be_nil
-  end
-
-  it 'has a locale value' do 
-    expect(Wowrb.config.locale).not_to be_empty
-    expect(Wowrb.config.locale).not_to be_nil
-  end 
-
   it 'has not a key value' do     
     expect(Wowrb.region_leaderboard)
       .to eq("Env variables missing: ENV['BATTLE_NET_KEY']")
