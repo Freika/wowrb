c4c0153dd3ce119f0abb3b2420e185619c4308b8
Freika <iamfrey@gmail.com>
2016-06-21 22:55:53 +0600

    Remove unnecessary spaces.

--- spec/wowrb_spec.rb
+++ spec/wowrb_spec.rb
@@ -1,26 +1,26 @@
 require 'spec_helper'
-    
+
 describe Wowrb do
   it 'has a version number' do
     expect(Wowrb::VERSION).not_to be_nil
   end
 
-  it 'has not a key value' do     
+  it 'has not a key value' do
     expect(Wowrb.region_leaderboard)
       .to eq("Env variables missing: ENV['BATTLE_NET_KEY']")
   end
 
-  it 'has not a region value' do     
+  it 'has not a region value' do
     expect(Wowrb.region_leaderboard)
       .to eq("Env variables missing: ENV['BATTLE_NET_REGION']")
   end
 
-  it 'has not a locale value' do     
+  it 'has not a locale value' do
     expect(Wowrb.region_leaderboard)
       .to eq("Env variables missing: ENV['BATTLE_NET_LOCALE']")
   end
 
-  it 'has not all value' do     
+  it 'has not all value' do
     expect(Wowrb.region_leaderboard)
       .to eq("Env variables missing: ENV['BATTLE_NET_KEY'], \
              ENV['BATTLE_NET_REGION'], ENV['BATTLE_NET_LOCALE']")
